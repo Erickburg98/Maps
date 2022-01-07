e9778188893a55fa4d89c82dfa0d2ea0e128d8e0
erick <erickburg_98@live.com.mx>
2022-01-07 17:09:16 -0600

    inicio

---
+++ js/coverflow/coverflow.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('.coverflow-slider').flipster({
+        scrollwheel: false,
+        // [true|false]
+        // Enable mousewheel/trackpad navigation; up/left = previous, down/right = next
+
+        touch: false
+        // [true|false]
+        // Enable swipe navigation for touch devices
+
+    });
+
+});
+
+
+
